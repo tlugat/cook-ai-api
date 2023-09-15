3eb9471cb2d9cc0e894d9eeee114b781860bb6fa
tlugat <teo.lugat@ausha.co>
2023-09-15 12:17:34 +0200

    ✨ OpenAI Service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,13 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  ParseUUIDPipe,
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseGuards,
+    ParseUUIDPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -17,45 +17,41 @@ import { User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    const user = await this.usersService.create(createUserDto);
-    return user;
-  }
+    @Post()
+    async create(@Body() createUserDto: CreateUserDto) {
+        const user = await this.usersService.create(createUserDto);
+        const { password, ...result } = user;
+        return result;
+    }
 
-  @Get()
-  @UseGuards(JwtAuthGuard)
-  async findAll() {
-    const users = await this.usersService.findAll();
-    return users;
-  }
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    async findAll() {
+        const users = await this.usersService.findAll();
+        return users;
+    }
 
-  @Get(':id')
-  @UseGuards(JwtAuthGuard)
-  async findOne(
-    @Param('id', ParseUUIDPipe) id: string,
-  ): Promise<Omit<User, 'password'>> {
-    const user = await this.usersService.findOne(id);
-    const { password, ...result } = user;
-    return result;
-  }
+    @Get(':id')
+    @UseGuards(JwtAuthGuard)
+    async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<Omit<User, 'password'>> {
+        const user = await this.usersService.findOne(id);
+        const { password, ...result } = user;
+        return result;
+    }
 
-  @Patch(':id')
-  @UseGuards(JwtAuthGuard)
-  async update(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
-    const user = await this.usersService.update(id, updateUserDto);
-    return user;
-  }
+    @Patch(':id')
+    @UseGuards(JwtAuthGuard)
+    async update(@Param('id', ParseUUIDPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
+        const user = await this.usersService.update(id, updateUserDto);
+        return user;
+    }
 
-  @Delete(':id')
-  @UseGuards(JwtAuthGuard)
-  async remove(@Param('id', ParseUUIDPipe) id: string) {
-    const user = await this.usersService.remove(id);
-    return user;
-  }
+    @Delete(':id')
+    @UseGuards(JwtAuthGuard)
+    async remove(@Param('id', ParseUUIDPipe) id: string) {
+        const user = await this.usersService.remove(id);
+        return user;
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,44 +8,38 @@ export const roundsOfHashing = 10;
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const hashedPassword = await bcrypt.hash(
-      createUserDto.password,
-      roundsOfHashing,
-    );
+    async create(createUserDto: CreateUserDto) {
+        const hashedPassword = await bcrypt.hash(createUserDto.password, roundsOfHashing);
 
-    createUserDto.password = hashedPassword;
+        createUserDto.password = hashedPassword;
 
-    return this.prisma.user.create({
-      data: createUserDto,
-    });
-  }
-
-  findAll() {
-    return this.prisma.user.findMany();
-  }
-
-  findOne(id: string) {
-    return this.prisma.user.findUnique({ where: { id } });
-  }
-
-  async update(id: string, updateUserDto: UpdateUserDto) {
-    if (updateUserDto.password) {
-      updateUserDto.password = await bcrypt.hash(
-        updateUserDto.password,
-        roundsOfHashing,
-      );
+        return this.prisma.user.create({
+            data: createUserDto,
+        });
     }
 
-    return this.prisma.user.update({
-      where: { id },
-      data: updateUserDto,
-    });
-  }
+    findAll() {
+        return this.prisma.user.findMany();
+    }
 
-  remove(id: string) {
-    return this.prisma.user.delete({ where: { id } });
-  }
+    findOne(id: string) {
+        return this.prisma.user.findUnique({ where: { id } });
+    }
+
+    async update(id: string, updateUserDto: UpdateUserDto) {
+        if (updateUserDto.password) {
+            updateUserDto.password = await bcrypt.hash(updateUserDto.password, roundsOfHashing);
+        }
+
+        return this.prisma.user.update({
+            where: { id },
+            data: updateUserDto,
+        });
+    }
+
+    remove(id: string) {
+        return this.prisma.user.delete({ where: { id } });
+    }
 }
