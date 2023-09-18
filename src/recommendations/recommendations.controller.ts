87c7f3e312b15406e48c96727e9f9939b0249cf2
tlugat <teo.lugat@ausha.co>
2023-09-18 20:17:42 +0200

    ✨ Food preferences

--- src/recommendations/recommendations.controller.ts
+++ src/recommendations/recommendations.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { ChatCompletionMessageParam } from 'openai/resources/chat';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { OpenaiService } from 'src/openai/openai.service';
@@ -14,12 +14,19 @@ export class RecommendationsController {
     ) {}
 
     @Post()
-    async create(@Body() recipeDto: CreateRecommendationsDto): Promise<string[]> {
+    async create(@Body() recipeDto: CreateRecommendationsDto, @Req() request): Promise<string[]> {
+        const { foodPreferences } = request.user;
+        const userFoodPreferencesPrompt = `
+        You should definitely consider the following food preferences if they are relevant: ${foodPreferences}
+        `;
+        const systemMessageContent =
+            'You\'re a chef for 15years and you\'re going to help users to get 3 relevant and the most similar as possible recipes from a recipe title and description. For every request you will send a JSON object following this format: {"recommendations": ["", "", ""]}}. Each should be a recipe title only.';
         const recipe = await this.recipesService.findOneById(recipeDto.recipeId);
         const SYSTEM_MESSAGE: ChatCompletionMessageParam = {
             role: 'system',
-            content:
-                'You\'re a chef for 15years and you\'re going to help users to get 3 relevant and the most similar as possible recipes from a recipe title and description. For every request you will send a JSON object following this format: {"recommendations": ["", "", ""]}}. Each should be a recipe title only.',
+            content: foodPreferences
+                ? systemMessageContent + userFoodPreferencesPrompt
+                : systemMessageContent,
         };
         const message = `title: ${recipe.title}\ndescription: ${recipe.description}`;
         const messages: ChatCompletionMessageParam[] = [
